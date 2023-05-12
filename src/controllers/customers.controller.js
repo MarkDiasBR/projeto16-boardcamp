05f178f939aa9c732aeb909f1af2118e9b1fe894
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-05-12 03:54:21 -0300

    PUT /customers/:id

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -3,7 +3,7 @@ import { db } from '../app.js';
 export async function getCustomers(req, res) {
 
     try {
-        const response = await db.query(`SELECT * FROM customers`)
+        const response = await db.query(`SELECT * FROM customers`);
         res.send(response.rows);
     } catch (err) {
         res.status(500).send(`🚫 Unexpected server error!\n\n${err.message}`);
@@ -15,7 +15,7 @@ export async function getCustomerById(req, res) {
     const { id } = req.params;
 
     try {
-        const response = await db.query(`SELECT * FROM customers WHERE id=$1`, [id])
+        const response = await db.query(`SELECT * FROM customers WHERE id=$1`, [id]);
         res.send(response.rows);
     } catch (err) {
         res.status(500).send(`🚫 Unexpected server error!\n\n${err.message}`);
@@ -28,8 +28,22 @@ export async function postCustomer(req, res) {
 
     try {
         await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1,$2,$3,$4)`,
-        [name, phone, cpf, birthday])
+        [name, phone, cpf, birthday]);
         res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(`🚫 Unexpected server error!\n\n${err.message}`);
+    }
+}
+
+export async function updateCustomerById(req, res) {
+
+    const { id } = req.params;
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+        await db.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5`,
+        [name, phone, cpf, birthday, id]);
+        res.sendStatus(200);
     } catch (err) {
         res.status(500).send(`🚫 Unexpected server error!\n\n${err.message}`);
     }
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getCustomers, getCustomerById, postCustomer } from '../controllers/customers.controller.js';
+import { getCustomers, getCustomerById, postCustomer, updateCustomerById } from '../controllers/customers.controller.js';
 
 const customersRouter = Router();
 
 customersRouter.get('/customers', getCustomers);
 customersRouter.get('/customers/:id', getCustomerById);
 customersRouter.post('/customers', postCustomer);
+customersRouter.put('/customers/:id', updateCustomerById);
 
 export default customersRouter;
